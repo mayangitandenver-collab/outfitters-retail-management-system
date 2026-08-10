3e8a3233f6279d4dad9e4f018d3d375baa724f58
mayangitandenver-collab <mayangitandenver@gmail.com>
2026-08-10 19:48:11 +0800

    Remove redundant Blazor authorization registration

--- src/Outfitters.Web/Program.cs
+++ src/Outfitters.Web/Program.cs
@@ -10,7 +10,6 @@ builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 builder.Services
     .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie();
-builder.Services.AddAuthoriztionCore();
 builder.Services.AddScoped<AuthSession>();
 builder.Services.AddScoped<BearerTokenHandler>();
 builder.Services.AddScoped<AuthenticationStateProvider, OrmsAuthenticationStateProvider>();
